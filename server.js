040d4f0576354c85cb88b207765b78ee0dc01697
Hsiangyun Yang <abel91740@gmail.com>
2018-01-20 03:58:10 -0500

    heroku update

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-var PORT = 3000;
+var PORT = var PORT = process.env.PORT||3000;
 
 // Initialize Express
 var app = express();
@@ -29,7 +29,8 @@ app.use(express.static("public"));
 // Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+// var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+var MONGODB_URI ="mongodb://heroku_zx21rn5k:m3ffajeuvpm5v4p45akg1jj9vb@ds263707.mlab.com:63707/heroku_zx21rn5k";
 mongoose.connect(MONGODB_URI, {
 // mongoose.connect("mongodb://localhost/week18Populater", {
   useMongoClient: true
